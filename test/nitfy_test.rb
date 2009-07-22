20bf7215b7d83406de7e4d41135ac5b458a12ced
robl <robl@monkeyhelper.com>
2009-07-22 21:39:55 +0100

    Added contexts for tests

--- test/nitfy_test.rb
+++ test/nitfy_test.rb
@@ -1,19 +1,34 @@
 require 'test_helper'
 
 class NiftyInvalidDocument < Test::Unit::TestCase
-  doc = Nitfy::Document.parse('<nitf></nitf>', :single => true)
-  should "have not have a header" do
-    doc.head.nil?
+
+  context "An empty NIFT file" do
+
+    setup do
+      @doc = Nitfy::Document.parse('<nitf></nitf>', :single => true)
+    end
+
+    should "have not have a header" do
+      @doc.head.nil?
+    end
+
   end
 end
 
 class NitfyTest < Test::Unit::TestCase
-  doc = Nitfy::Document.parse(fixture_file('fishing.nitf'), :single => true)
-  should "have a header" do
-    !doc.head.nil?
-  end
 
-  should "have a title" do
-    doc.head.title == 'Norfolk Weather and Tide Updates'
+  context "A NIFT file" do
+
+    setup do
+      @doc = Nitfy::Document.parse(fixture_file('fishing.nitf'), :single => true)
+    end
+
+    should "have a header" do
+      !@doc.head.nil?
+    end
+
+    should "have a title" do
+      @doc.head.title == 'Norfolk Weather and Tide Updates'
+    end
   end
 end
