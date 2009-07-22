fee7a686fde88f370e4b9e253c86e02d0738cd3a
robl <robl@monkeyhelper.com>
2009-07-22 21:25:47 +0100

    Added some tests

--- test/nitfy_test.rb
+++ test/nitfy_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
+class NiftyInvalidDocument < Test::Unit::TestCase
+  doc = Nitfy::Document.parse('<nitf></nitf>', :single => true)
+  should "have not have a header" do
+    doc.head.nil?
+  end
+end
+
 class NitfyTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  doc = Nitfy::Document.parse(fixture_file('fishing.nitf'), :single => true)
+  should "have a header" do
+    !doc.head.nil?
+  end
+
+  should "have a title" do
+    doc.head.title == 'Norfolk Weather and Tide Updates'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,9 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'nitfy'
 
+def fixture_file(filename)
+  File.read(File.dirname(__FILE__) + "/fixtures/#{filename}")
+end
+
 class Test::Unit::TestCase
 end
